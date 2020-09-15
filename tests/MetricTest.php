a57ae2352a5dbbd2ec8bf7b65d28a876a8c7d3e3
Jeff Sturek <jsturek8@unl.edu>
2020-09-15 12:51:27 -0500

    Update test

--- tests/MetricTest.php
+++ tests/MetricTest.php
@@ -8,13 +8,34 @@ class MetricTest extends \PHPUnit\Framework\TestCase
     /**
      * @test
      */
-    public function getHTMLVersion()
+    public function getSensitiveTermsOccurrences()
     {
-        $metric = new Metric('unl');
+        $metric = new Metric('sensitive_terms');
 
         $xpath = $this->getTestXPath('sensitive_terms.html');
-        $results = array();
-        $this->assertEquals($results, $metric->getSensitiveTermsOccurrences($xpath));
+        $result = $metric->getSensitiveTermsOccurrences($xpath);
+        $expectedResult = array(
+            array(
+                'value_found' => 'crazy',
+                'count' => 1,
+                'context' => 'SiteMaster crazy testing site'
+            ),
+            array(
+                'value_found' => 'insane',
+                'count' => 1,
+                'context' => "This insane site just contains a few pages to help with integration testing of SiteMaster"
+            )
+        );
+
+        $this->assertEquals(count($expectedResult), count($result));
+
+        $this->assertEquals($expectedResult[0]['value_found'], $result[0]['value_found']);
+        $this->assertEquals($expectedResult[0]['count'], $result[0]['count']);
+        $this->assertEquals($expectedResult[0]['context'], $result[0]['context']);
+
+        $this->assertEquals($expectedResult[1]['value_found'], $result[1]['value_found']);
+        $this->assertEquals($expectedResult[1]['count'], $result[1]['count']);
+        $this->assertEquals(trim($expectedResult[1]['context']), trim($result[1]['context']));
     }
 
     public function getTestXPath($filename)
